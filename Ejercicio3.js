9a9fdd549c1bfafbfc94187206ab4035e9558a17
JeanChara <jcharaco@unsa.edu.pe>
2023-05-18 18:03:30 -0500

    se creeo las bariables para que se haga posible la programacion en js del ejercicio3

---
+++ Ejercicio3.js
@@ -0,0 +1,6 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const seconds = document.getElementById("seconds");
+
+
