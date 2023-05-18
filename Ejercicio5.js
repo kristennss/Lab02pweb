9a9682db1916a80a0db1d7b8639951fd9bb2273d
KristenFer <kfernandezca@unsa.edu.pe>
2023-05-18 10:09:07 -0500

    Obtenemos las etiquetas para que el html lo reconozca y tambien un bucle for para la creacion de la tabla

--- Ejercicio5.js
+++ Ejercicio5.js
@@ -4,9 +4,15 @@ let sumatoria = 0;
 const numerosTabla = [];
 
 function generarTabla(){
-
+    numElementos = document.getElementById("numElemntos").value;
+    console.log(numElementos);
+    for (let i = 0; i < numElementos; i++) {
+      numerosTabla[i] = Math.floor(Math.random() * 200);
+    }
+    console.log(numerosTabla); 
+    let j = 0;
 }
 
 function sumar(){
-    
+
 }
