b946ff172295e870396324232d76e5d3a0a53d4f
JeanChara <jcharaco@unsa.edu.pe>
2023-05-18 18:04:02 -0500

    se comleto el ejercicio numero 3

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -3,4 +3,25 @@ const hours = document.getElementById("hours");
 const mins = document.getElementById("mins");
 const seconds = document.getElementById("seconds");
 
+const aqp = '15 Aug 2023';
 
+function countTimer(){
+  const aqpDate = new Date(aqp);
+  const ahora = new Date();
+
+  const totalSeconds = (aqpDate - ahora) / 1000;
+
+  const daysT = Math.floor(totalSeconds / 3600 / 24);
+  const hoursT = Math.floor(totalSeconds / 3600) % 24; 
+  const minsT = Math.floor(totalSeconds / 60) % 60;
+  const secondsT = Math.floor(totalSeconds % 60);
+
+
+  days.innerHTML = daysT;
+  hours.innerHTML = hoursT;
+  mins.innerHTML = minsT;
+  seconds.innerHTML = secondsT;
+}
+
+countTimer();
+setInterval(countTimer, 1000);
