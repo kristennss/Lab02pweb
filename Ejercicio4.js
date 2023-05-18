c7ff10638b1d558361bc1396032aec03c9bfd21c
KristenFer <kfernandezca@unsa.edu.pe>
2023-05-18 09:47:40 -0500

    Obtenemos las etiquetas con id para que el html lo reconozca y asu vez optenemos los valores y rastreamos el input

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -4,5 +4,8 @@ let url = ""
 let codigo= ""
 
 function returnCodigo(){
-    
+    let box = document.getElementById("box-content")
+    input = document.getElementById("input")
+    url = input.value
+    input.value = ""
 }
