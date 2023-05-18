1b694b8acb97f06fe488a1b0d779aae351137f66
KristenFer <kfernandezca@unsa.edu.pe>
2023-05-18 10:12:39 -0500

    Creamos otro for para que la tabla tenga la forma dada por el usuario

--- Ejercicio5.js
+++ Ejercicio5.js
@@ -11,6 +11,16 @@ function generarTabla(){
     }
     console.log(numerosTabla); 
     let j = 0;
+    for (let i = 0; i < numerosTabla.length; i++) {
+        miTabla = miTabla + "<th>"+numerosTabla[i]+"</th>";
+        j++;
+        if(j % 5 == 0) {
+          miTabla = miTabla + "</tr><tr>"
+        }
+      }
+      miTabla = miTabla + "</tr></table>"
+      let modificar = document.getElementById("modificar");
+      modificar.innerHTML = miTabla + "<div class='centrado'> <br><p id = 'boton'><button onclick='sumar()'>Sumar</button></p><p id='suma'></p></div>";
 }
 
 function sumar(){
